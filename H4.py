3433aca6f3fff800e31e9d526466387f66c95fe2
Kahuur <115454898+Kahuur@users.noreply.github.com>
2022-10-19 09:34:50 +0300

    Add files via upload

--- H4.py
+++ H4.py
@@ -1,8 +1,91 @@
 # T.Lehtsalu
 # 17.10.2022
 # Harjutus04
+import random
+
+#ruutude ja kuupide tabel
+number=1
+for i in range (1,11):
+    print(number,end="  ")
+    print(number**2,end="  ")
+    print(number**3,end="  ")
+    print()
+    number+=1
+
+#pank
+kontoraha = 10000
+intress = 0.05 #5%
+aastad = 6
+x=1
+for i in range (aastad):
+    print(f"Sul on raha pangas {x} aastat")
+    print(f"arvel on{kontoraha}")
+    kontoraha+=kontoraha*intress
+    x+=1
+
+#arvamismäng
+loop = 1
+
+while loop==1:
+    arv = random.randint(1,10)
+    print(arv) #testimiseks
+    for i in range(3):
+        pakkumine = int(input("Arva arv 1-10: "))
+        if pakkumine==arv:
+            print("LETS GOOOO")
+            break
+        else:
+            print("Better luck next time buddy")
+    loop = int(input("Jätka 1/0: "))
+print("Game over")
+        
+
+#korrutustabel
+arv = 5
+for i in range(10):
+    print(f"{arv} x {i} = {arv*i}")
+
+
+#paarisjapaaritu
+for i in range(1,10):
+    if i%2==0:
+        print(f"{i} paaris ")
+    else:
+        print(f"{i} paaritu")
+    
+    
+
+#loto
+for i in range(5):
+    print(random.randint(0,9),end="")
 
 # Jalgpalli meeskond
+
+sugu = input("\nTeie sugu: ")
+if sugu=="mees":
+    vanus = int(input("Teie vanus: "))
+    if 16<=vanus<=18:
+        print("Kõlbate tiimi")
+else:
+    print("Headaega")
+
+#tärnid
+
+print()
+x=1
+for i in range (5):
+    print("* "*5)
+    
+print()
+for i in range (5):
+    print("* "*x)
+    x+=1
+    
+print()
+y=5
+for i in range (5):
+    print("* "*y)
+    y-=1
 
 
 # müük
@@ -37,3 +120,5 @@ if a==b:
     print(f"{a} ja {b} teevad kokku ruudu")
 else:
     print(f"{a} ja {b} teevad kokku ristküliku")
+ 
+ 
